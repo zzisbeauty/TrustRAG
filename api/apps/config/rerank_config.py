6092c96735d31b8f609c5a00eba2e12d283e184a
yanqiangmiffy <1185918903@qq.com>
2025-01-21 12:05:23 +0800

    update

--- api/apps/config/rerank_config.py
+++ api/apps/config/rerank_config.py
@@ -11,4 +11,4 @@
 """
 class RerankConfig():
     model_name_or_path:str="/data/users/searchgpt/pretrained_models/bge-reranker-large"
-    llm_url:str="http://10.208.63.29:8888"
+    llm_url:str="http://10.208.63.29:8891"
